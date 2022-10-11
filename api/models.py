3c663943c41d93e4fe176f9888fd726e6a469e72
Rahmaao <rahma.olaniyan@aun.edu.ng>
2022-10-11 12:07:24 +0100

    Made models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,41 @@
+from email.policy import default
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    price = models.FloatField
+    description = models.TextField
+    # photo = models.ImageField
+
+
+class MenuFoodItem(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    stock = models.IntegerField
+
+
+class OrderItem(models.Model):
+    fooditem = models.ForeignKey(MenuFoodItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+
+class Order(models.Model):
+    client_name = models.CharField(max_length=100)
+    client_phone = models.CharField(max_length=100)
+    client_email = models.EmailField
+    # orderitem = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    total_charge = models.FloatField    
+    client_location = models.TextField
+    status = models.BooleanField(default=False)
+
+    class OrderStatus(models.TextChoices):
+        SETTLED = 'STLD', _('Settled')
+        FAILED = 'FAIL', _('Failed')
+        Pending = 'PEND', _('Pending')
+
+
+
+
+    
